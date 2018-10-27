47e68e4b89bf90e5afc154ffe453e78052a2473e
NareshReddy11 <nareshreddy.v1994@gmail.com>
2018-10-27 21:07:58 +0530

    Networking

---
+++ Client1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProgramingClient
+{
+    class Client1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 800);
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                string s = string.Empty;
+                while (!s.Equals("Exit"))
+                {
+                    Console.WriteLine("Enter Data :");
+                    s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+                    string sw = reader.ReadLine();
+                    Console.WriteLine(sw);
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
